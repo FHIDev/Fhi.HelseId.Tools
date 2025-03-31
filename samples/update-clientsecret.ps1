aa91c66bfb081c0497da24d6ccdc766d9eb44c84
Bjørn-Vegard Thoresen <bjorn-vegard.thoresen@fhi.no>
2025-03-31 12:16:21 +0200

    Update name in samples to HelseIdSelvbetjening

--- samples/update-clientsecret.ps1
+++ samples/update-clientsecret.ps1
@@ -1,4 +1,4 @@
-﻿$exePath = "..\src\Fhi.HelseIdSelvebetjening.CLI\bin\Debug\net9.0\Fhi.HelseIdSelvebetjening.CLI.exe"
+﻿$exePath = "..\src\Fhi.HelseIdSelvbetjening.CLI\bin\Debug\net9.0\Fhi.HelseIdSelvbetjening.CLI.exe"
 $clientId = ""
 $newKey = ""
 $oldKey =""
