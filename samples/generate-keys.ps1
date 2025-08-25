e2a9252671a27ce3680b319cf43b7d9357a70413
Kattemat <tuvakleberg@gmail.com>
2025-08-25 13:45:53 +0200

    small changes to samples

--- samples/generate-keys.ps1
+++ samples/generate-keys.ps1
@@ -1,6 +1,4 @@
 dotnet tool install --global Fhi.HelseIdSelvbetjening.CLI --version 1.0.0-beta1
 
-$env:DOTNET_ENVIRONMENT = "Test"
-& helseid-cli generatekey --KeyFileNamePrefix "prefixname" --KeyDirectory "C:\\temp"
-Write-Host $output
+helseid-cli generatekey --KeyFileNamePrefix "prefixname" --KeyDirectory "C:\\temp"
    
