76bdfab798c9886caf6bbca63e69b977192cf081
Kattemat <tuvakleberg@gmail.com>
2025-10-28 09:17:28 +0100

    working script and dockerfile

--- docker/Key-gen-and-update.sh
+++ docker/Key-gen-and-update.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
-echo "Starting Script!"
-echo "Installing tool..."
-dotnet --version
-#dotnet tool install --global Fhi.HelseIdSelvbetjening.CLI --version 1.0.0
-dotnet tool install --global Fhi.HelseIdSelvbetjening.CLI --version 1.0.0-beta1
-echo "Tool installed."
-ClientId="5c052f47-f25b-486f-a766-ebd93ce1a789"
-KeyName="HelseIdJWK"
-NewKeyDirectory="C:/NewHelseIDJWKKey"
-CurrentKeyDirectory="C:/CurrentHelseIDJWKKey"
-NewKeyPath="${KeyDirectory}/${KeyName}_public"
-CurrentKeyPath="${CurrentKeyDirectory}/${KeyName}_private"
-Authority="https://helseid-sts.test.nhn.no"
-BaseAddress="https://api.selvbetjening.test.nhn.no"
-echo "Generating key..."
-helseid-cli generatejsonwebkey --KeyFileNamePrefix $ClientId --KeyDirectory $NewKeyDirectory
-echo "Keys generated."
-echo "Updating keys in HelseID...."
+echo "Starting Script!";
+dotnet --version;
+ClientId="$CLIENT_ID";
+KeyName="HelseIdJWK";
+NewKeyDirectory="/script/NewHelseIDJWKKey";
+CurrentKeyDirectory="/script/CurrentHelseIDJWKKey";
+NewKeyPath="${NewKeyDirectory}/${KeyName}_public";
+CurrentKeyPath="${CurrentKeyDirectory}/${KeyName}_private";
+Authority="https://helseid-sts.test.nhn.no";
+BaseAddress="https://api.selvbetjening.test.nhn.no";
+echo "Generating key...";
+helseid-cli generatejsonwebkey --KeyFileNamePrefix $ClientId --KeyDirectory $NewKeyDirectory;
+echo "Keys generated.";
+echo "Updating keys in HelseID....";
 # helseid-cli updateclientkey --ClientId $ClientId --NewPublicJwkPath $NewKeyPath --ExistingPrivateJwkPath $CurrentKeyPath --Authority $Authority --BaseAddress $BaseAddress --yes
-echo "Keys updated in HelseID."
-echo "Deleting old keys..."
-#rm $CurrentKeyPath
-echo "Keys deleted."
-echo "Moving new keys into currently used keys folder..."
-#mv $NewKeyPath $CurrentKeyPath
-echo "Keys moved."
-echo "Job completed!"
+echo "Keys updated in HelseID.";
+echo "Update keys in Keyvault...";
+# KEYVAULT
+az --version;
+echo "Keys updated in keyvault.";
+echo "Job completed!";
